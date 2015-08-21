7802425fd97f0cf88476cab76584d694ad6a0c38
Timothee Emery <tim@youshould.eu>
2015-08-21 02:00:17 +0200

    no query string for url to first page

--- index.js
+++ index.js
@@ -17,77 +17,82 @@ exports = module.exports;
 
 exports.href = function href(req) {
 
-  return function(prev, params) {
+    return function(prev, params) {
 
-    var query = _.clone(req.query);
+        var query = _.clone(req.query);
+        console.dir(query.page);
+        console.dir(prev);
 
-    if (typeof prev === 'object') {
-      params = prev;
-      prev = false;
-    } else {
-      prev = (typeof prev === 'boolean') ? prev : false;
-      query.page = prev ? query.page-= 1 : query.page += 1;
-      query.page = (query.page < 1) ? 1 : query.page;
-    }
+        if(query.page == 2 && prev == true)
+            return url.parse(req.originalUrl).pathname;
 
-    // allow overriding querystring params
-    // (useful for sorting and filtering)
-    // another alias for `_.assign` is `_.extend`
-      delete query.limit;
-    if (_.isObject(params))
-      query = _.assign(query, params);
+        if (typeof prev === 'object') {
+            params = prev;
+            prev = false;
+        } else {
+            prev = (typeof prev === 'boolean') ? prev : false;
+            query.page = prev ? query.page-= 1 : query.page += 1;
+            query.page = (query.page < 1) ? 1 : query.page;
+        }
 
-    return (query.page === 2 && prev === true) ? url.parse(req.originalUrl).pathname : url.parse(req.originalUrl).pathname + '?' + querystring.stringify(query);
+        // allow overriding querystring params
+        // (useful for sorting and filtering)
+        // another alias for `_.assign` is `_.extend`
+        delete query.limit;
+        if (_.isObject(params))
+            query = _.assign(query, params);
 
-  };
+        return url.parse(req.originalUrl).pathname + '?' + querystring.stringify(query);
+
+    };
 };
 
 exports.hasNextPages = function hasNextPages(req) {
-  return function(pageCount) {
-    if (typeof pageCount !== 'number' || pageCount < 0)
-      throw new Error('express-paginate: `pageCount` is not a number >= 0');
-    return req.query.page < pageCount;
-  };
+    return function(pageCount) {
+        if (typeof pageCount !== 'number' || pageCount < 0)
+            throw new Error('express-paginate: `pageCount` is not a number >= 0');
+        return req.query.page < pageCount;
+    };
 };
 
 exports.middleware = function middleware(limit, maxLimit) {
 
-  var that = this;
+    var that = this;
 
-  that.limit = (typeof limit === 'number') ? parseInt(limit, 10) || 10 : 10;
+    that.limit = (typeof limit === 'number') ? parseInt(limit, 10) || 10 : 10;
 
-  that.maxLimit = (typeof maxLimit === 'number') ? parseInt(maxLimit, 10) || 10 : 50;
+    that.maxLimit = (typeof maxLimit === 'number') ? parseInt(maxLimit, 10) || 10 : 50;
 
-  if (that.limit < 1)
-    throw new Error('express-paginate: `limit` cannot be less than 1');
+    if (that.limit < 1)
+        throw new Error('express-paginate: `limit` cannot be less than 1');
 
-  if (that.maxLimit < 1)
-    throw new Error('express-paginate: `maxLimit` cannot be less than 1');
+    if (that.maxLimit < 1)
+        throw new Error('express-paginate: `maxLimit` cannot be less than 1');
 
-  return function _middleware(req, res, next) {
+    return function _middleware(req, res, next) {
 
-    req.query.page = (typeof req.query.page === 'string') ? parseInt(req.query.page, 10) || 1 : 1;
+        req.query.page = (typeof req.query.page === 'string') ? parseInt(req.query.page, 10) || 1 : 1;
 
-    req.query.limit = (typeof req.query.limit === 'string') ? parseInt(req.query.limit, 10) || that.limit : that.limit;
+        req.query.limit = (typeof req.query.limit === 'string') ? parseInt(req.query.limit, 10) || that.limit : that.limit;
 
-    if (req.query.limit > that.maxLimit)
-      req.query.limit = that.maxLimit;
+        if (req.query.limit > that.maxLimit)
+            req.query.limit = that.maxLimit;
 
-    if (req.query.page < 1)
-      req.query.page = 1;
+        if (req.query.page < 1)
+            req.query.page = 1;
 
-    if (req.query.limit < 1)
-      req.query.limit = 1;
+        if (req.query.limit < 1)
+            req.query.limit = 1;
 
-    res.locals.paginate = {};
-    res.locals.paginate.page = req.query.page;
-    res.locals.paginate.limit = req.query.limit;
-    res.locals.paginate.href = exports.href(req);
-    res.locals.paginate.hasPreviousPages = req.query.page > 1;
-    res.locals.paginate.hasNextPages = exports.hasNextPages(req);
+        res.locals.paginate = {};
+        res.locals.paginate.page = req.query.page;
+        res.locals.paginate.limit = req.query.limit;
+        res.locals.paginate.href = exports.href(req);
+        res.locals.paginate.hasPreviousPages = req.query.page > 1;
+        res.locals.paginate.hasNextPages = exports.hasNextPages(req);
 
-    next();
+        next();
 
-  };
+    };
 
 };
